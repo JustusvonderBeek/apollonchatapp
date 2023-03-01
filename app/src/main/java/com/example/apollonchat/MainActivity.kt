01a658740923a9dae7a44de5e6b4943aae309958
Justus <beek@in.tum.de>
2023-03-01 20:02:51 +0100

    Added navigation across fragments

--- app/src/main/java/com/example/apollonchat/MainActivity.kt
+++ app/src/main/java/com/example/apollonchat/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.apollonchat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.apollonchat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
     }
 }
