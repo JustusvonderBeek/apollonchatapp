06cd71de62e27e79b9f1d763c43a8558234e846f
Justus <beek@in.tum.de>
2023-03-07 18:26:21 +0100

    Added prototype of networking class

--- app/src/main/java/com/example/apollonchat/networking/Networking.kt
+++ app/src/main/java/com/example/apollonchat/networking/Networking.kt
@@ -1,21 +1,21 @@
 package com.example.apollonchat.networking
 
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
-import kotlinx.coroutines.Dispatchers
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.util.date.*
+import io.ktor.utils.io.jvm.javaio.*
+import kotlinx.coroutines.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import java.io.IOException
 import java.io.InputStream
-import java.io.OutputStream
 import java.net.InetAddress
-import java.net.InetSocketAddress
-import java.net.Socket
-import java.security.cert.X509Certificate
-import java.util.concurrent.Executors
-import javax.net.ssl.X509TrustManager
-import kotlin.coroutines.coroutineContext
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.BlockingQueue
 
-abstract class Networking(hostAddress : InetAddress) {
+object Networking {
 
     /* This class contains the logic to send data to the server
     * and retrieve answers back from the server.
@@ -30,37 +30,38 @@ abstract class Networking(hostAddress : InetAddress) {
     ----------------------------------------------------------------
      */
 
-    @Volatile
-    var INSTANCE : Networking? = null
-
-    val remoteAddress : InetAddress = hostAddress
-    lateinit var inputStream : InputStream
-    lateinit var outputStream : OutputStream
+    lateinit var remoteAddress : InetAddress
+    lateinit var inputQueue : BlockingQueue<ByteArray>
+    lateinit var outputQueue : BlockingQueue<ByteArray>
     lateinit var socket : Socket
+    var started : Boolean = false
 
-    /*
-    ----------------------------------------------------------------
-    Singleton
-    ----------------------------------------------------------------
-     */
+    private var networkingJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + networkingJob)
+    private val ioScope = CoroutineScope(Dispatchers.IO + networkingJob)
 
-    fun getInstance(hostAddress: InetAddress) : Networking {
-        var instance = INSTANCE
-        if(instance == null) {
-            instance = this
-        }
-        return instance
-    }
-
-    fun write(byteArray: ByteArray) {
+    fun write(data : Message) {
         try {
-            outputStream.write(byteArray)
+            val sData = Json.encodeToString(data)
+            val rawPacketData = sData.toByteArray(charset = Charsets.UTF_8)
+            val packet = (rawPacketData.size + 2).to2ByteArray() + rawPacketData
+            outputQueue.put(packet)
         } catch (ex : IOException) {
             ex.printStackTrace()
         }
     }
 
-    fun start() {
+    fun start(remoteAddress: InetAddress) {
+        this.remoteAddress = remoteAddress
+        this.outputQueue = ArrayBlockingQueue(20)
+        this.inputQueue = ArrayBlockingQueue(20)
+
+        if (this.started) {
+            Log.i("Networking", "Already started the network...")
+            return
+        } else {
+            this.started = true
+        }
 //        try {
 //            socket = Socket()
 //            socket.connect(InetSocketAddress(remoteAddress, 15467), 1000)
@@ -74,9 +75,12 @@ abstract class Networking(hostAddress : InetAddress) {
 //        val executor = Executors.newSingleThreadExecutor()
 //        var handler = Handler(Looper.getMainLooper())
 //
-//        val selManager = SelectorManger(Dispatchers.IO)
-//        socket = aSocket(selManager).tcp().connect(remoteAddress)
-        // The TLS variant
+        uiScope.launch {
+            withContext(Dispatchers.IO) {
+                val selManager = SelectorManager(Dispatchers.IO)
+                socket = aSocket(selManager).tcp().connect("192.168.2.10", port = 50000)
+
+                // The TLS variant
 //        socket = aSocket(selManager).tcp().connect(remoteAddress).tls(coroutineContext = coroutineContext) {
 //            trustManager = object : X509TrustManager {
 //                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
@@ -84,25 +88,82 @@ abstract class Networking(hostAddress : InetAddress) {
 //                override fun checkServerTrusted(p0: Array<out X509Certificate>?, p1: String?) {}
 //            }
 //        }
-        Log.i("Networking", "Listing on ...")
+//                Log.i("Networking", "Listing on ...")
 
-        startSending()
-        startListening()
+                startSending()
+                startListening()
+            }
+
+        }
+
     }
 
-    private fun startSending() {
-        // TODO: move to own thread or handling inside of new thread
-//        val sendChannel = socket.openWriteChannel()
-        while(true) {
-            val data = byteArrayOf()
-            // Fetch data from the internal queue
-//            sendChannel.write(data)
-//            sendChannel.flush()
+    private fun ByteArray.toHexString() = joinToString("") { "%02x".format(it) }
+    private fun Int.to2ByteArray() : ByteArray = byteArrayOf(shr(8).toByte(), toByte())
+
+    private suspend fun startSending() {
+        Log.i("Networking", "Starting to send...")
+        ioScope.launch {
+            val sendChannel = socket.openWriteChannel(autoFlush = true)
+            while(true) {
+                val packet = withContext(Dispatchers.IO) {
+                    return@withContext outputQueue.take()
+                }
+                // TODO: Fetch data from queue
+//                val message = Message(Category = 0x2, Type = 0x1, UserId = 12345U, MessageId = 814223U, ContactUserId = 54321U, Timestamp = getTimeMillis().toString(), Part = 0U, Message = "Das ist eine erste Nachricht :)")
+//                val sMessage = Json.encodeToString(message)
+//                val rawMessage = sMessage.toByteArray(charset("UTF-8"))
+//                val length = (rawMessage.size + 2).to2ByteArray()
+//                val packet = length + rawMessage
+
+//                Log.i("Networking", "Sending: $message \n ${packet.toHexString()}")
+                Log.i("Networking", "Sending: ${packet.toHexString()}")
+                withContext(Dispatchers.IO) {
+                    sendChannel.toOutputStream().write(packet)
+                }
+            }
         }
     }
 
-    private fun startListening() {
+    private var json = Json { ignoreUnknownKeys = true }
 
+    private fun startListening() {
+        Log.i("Networking", "Starting to receive...")
+        ioScope.launch {
+            val recChannel = socket.openReadChannel().toInputStream()
+            val sizeBuffer = ByteArray(2)
+            while(true) {
+                var read = 0
+                withContext(Dispatchers.IO) {
+                    read += recChannel.read(sizeBuffer)
+//                    while(read < 2) {
+//                        recChannel.read(sizeBuffer, read, sizeBuffer.size - read)
+//                    }
+                }
+//                val size = (sizeBuffer[0].toInt() shl 8) and sizeBuffer[1].toInt()
+                val size = sizeBuffer[1].toInt() - 2
+                Log.i("Networking", "Size expected: $size - ${sizeBuffer.toHexString()} - ${sizeBuffer[1]}")
+                val packetBuffer = ByteArray(size)
+                withContext(Dispatchers.IO) {
+                    read = recChannel.read(packetBuffer)
+                }
+                // Save the read data into a consumer queue for anthoer thread to handle
+                // Convert the packet to String and give to JSON to handle
+                val sPacket = packetBuffer.toString(Charsets.UTF_8)
+                val header = json.decodeFromString<Header>(sPacket)
+                // TODO: Decode
+                Log.i("Networking", "Received cat: ${header.Category}, type: ${header.Type}")
+
+            }
+        }
     }
+
+
+
+    /*
+    ----------------------------------------------------------------
+    Singleton
+    ----------------------------------------------------------------
+     */
 
 }
