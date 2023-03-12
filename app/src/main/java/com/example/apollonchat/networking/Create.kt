0c5ee68d8742b3c091e5bb9fdd8955fb018f8f1f
justus <beek@in.tum.de>
2023-03-12 17:59:47 +0100

    Updated constructors to include category and type

--- app/src/main/java/com/example/apollonchat/networking/Create.kt
+++ app/src/main/java/com/example/apollonchat/networking/Create.kt
@@ -2,9 +2,15 @@ package com.example.apollonchat.networking
 
 @kotlinx.serialization.Serializable
 data class Create(
-    var Category : Byte = 0x1,
-    var Type : Byte = 0x2,
+    var Category : Byte,
+    var Type : Byte,
     var UserId : UInt,
     var MessageId : UInt,
     var Username : String,
-)
+) {
+    constructor(
+        UserId: UInt,
+        MessageId: UInt,
+        Username: String
+    ) : this(0x01, 0x01, UserId, MessageId, Username)
+}
--- app/src/main/java/com/example/apollonchat/networking/Message.kt
+++ app/src/main/java/com/example/apollonchat/networking/Message.kt
@@ -1,7 +1,6 @@
 package com.example.apollonchat.networking
 
 import io.ktor.util.date.*
-
 @kotlinx.serialization.Serializable
 data class Message(
     var Category : Byte,
@@ -12,4 +11,14 @@ data class Message(
     var Timestamp : String,
     var Part : UInt,
     var Message : String,
-)
+) {
+    // TODO: Fix time timestamp to be generated internally and have the correct format
+    constructor(
+        UserId: UInt,
+        MessageId: UInt,
+        ContactUserId: UInt,
+        Timestamp: String,
+        Part: UInt,
+        Message: String
+    ) : this(0x02, 0x01, UserId, MessageId, ContactUserId, Timestamp, Part, Message)
+}
