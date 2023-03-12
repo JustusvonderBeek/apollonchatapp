ec5e0303de483111442331ac3050176489b00e48
justus <beek@in.tum.de>
2023-03-12 10:59:38 +0100

    Added back button to action bar

--- app/src/main/java/com/example/apollonchat/MainActivity.kt
+++ app/src/main/java/com/example/apollonchat/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.apollonchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.example.apollonchat.databinding.ActivityMainBinding
+import com.google.android.material.appbar.AppBarLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +19,13 @@ class MainActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        // This does only work when the Navigation component is inside a <fragment> tag, NOTHING ELSE!
+        val navController = findNavController(R.id.navHostFragment)
 
+        // Decide what fragments should be "top-level" and SHOULD NOT! have a "back" arrow
+        val appBarConfiguration = AppBarConfiguration.Builder(setOf(R.id.navigation_chat_list, R.id.navigation_chat_view, R.id.navigation_add_contact)).build()
+        // The back arrow in the actionBar (title bar on top)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
