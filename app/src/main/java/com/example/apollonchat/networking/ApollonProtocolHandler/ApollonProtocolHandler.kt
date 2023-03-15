31ae544cbf7a19d7349614d2bcbbbd76e01e7102
justus <beek@in.tum.de>
2023-03-15 20:09:34 +0100

    Added new class to handle protocol

---
+++ app/src/main/java/com/example/apollonchat/networking/ApollonProtocolHandler/ApollonProtocolHandler.kt
@@ -0,0 +1,6 @@
+package com.example.apollonchat.networking.ApollonProtocolHandler
+
+class ApollonProtocolHandler {
+    // TODO: Implement the Apollon Protocol here and group all operations
+    // in a single place to keep it concise and easy to understand
+}
