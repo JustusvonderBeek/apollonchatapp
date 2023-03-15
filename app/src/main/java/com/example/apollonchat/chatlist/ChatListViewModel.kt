39b97e1d0da31bcabad965c1ef8e1110d6f838a0
justus <beek@in.tum.de>
2023-03-15 15:57:42 +0100

    Handling contact question

--- app/src/main/java/com/example/apollonchat/chatlist/ChatListViewModel.kt
+++ app/src/main/java/com/example/apollonchat/chatlist/ChatListViewModel.kt
@@ -12,9 +12,12 @@ import com.example.apollonchat.database.message.MessageDao
 import com.example.apollonchat.database.user.User
 import com.example.apollonchat.database.user.UserDatabaseDao
 import com.example.apollonchat.networking.Networking
+import com.example.apollonchat.networking.constants.ContactType
 import com.example.apollonchat.networking.constants.DataType
 import com.example.apollonchat.networking.constants.PacketCategories
+import com.example.apollonchat.networking.packets.ContactOption
 import com.example.apollonchat.networking.packets.Message
+import com.example.apollonchat.networking.packets.NetworkOption
 import kotlinx.coroutines.*
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
@@ -56,6 +59,9 @@ class ChatListViewModel(val contactDatabase : ContactDatabaseDao, val userDataba
         uiScope.launch {
             registerCallbackForIncomingMessages()
         }
+        uiScope.launch {
+            registerCallbackForFriendRequests()
+        }
     }
 
     private suspend fun registerCallbackForIncomingMessages() {
@@ -72,9 +78,41 @@ class ChatListViewModel(val contactDatabase : ContactDatabaseDao, val userDataba
         }
     }
 
+    private suspend fun registerCallbackForFriendRequests() {
+        withContext(Dispatchers.IO) {
+            Networking.registerCallback(PacketCategories.CONTACT.cat.toLong(), ContactType.OPTION.type.toLong()) { packet ->
+                val option = json.decodeFromString<ContactOption>(packet)
+                var contactRequest = false
+                for (opt in option.Options) {
+                    when {
+                        opt.Type.contentEquals("Question") && opt.Value.contentEquals("Add") -> {
+                            // Other user wants to add us to the contact list
+                            // Accept by default for now and send answer back
+                            var userId = 1U
+                            if (_user.value != null) {
+                                userId = _user.value!!.userId.toUInt()
+                            }
+                            var username = "Username"
+                            if (_user.value != null) {
+                                username = _user.value!!.username
+                            }
+                            val answerOptions = listOf<NetworkOption>(NetworkOption("Answer", "Accept"), NetworkOption("Username", username))
+                            val answer = ContactOption(userId, option.UserId, answerOptions)
+                            Networking.write(answer)
+                            // TODO: Obtain friend information and add to list of contacts
+                            val newContact = Contact(option.UserId.toLong(), "I dont know", "drawable/usericon.png")
+                            contactDatabase.insertContact(newContact)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     private suspend fun startNetwork() {
         withContext(Dispatchers.IO) {
-            Networking.start(InetAddress.getByName("homecloud.homeplex.org"), contactDatabase, userDatabase, messageDatabase)
+            Networking.initialize(InetAddress.getByName("homecloud.homeplex.org"), contactDatabase, userDatabase, messageDatabase)
+            Networking.start()
         }
     }
 
@@ -85,9 +123,27 @@ class ChatListViewModel(val contactDatabase : ContactDatabaseDao, val userDataba
         }
     }
 
+    fun clearContacts() {
+        uiScope.launch {
+            clearAllContacts()
+        }
+    }
+
     private suspend fun clearCurrentUser() {
         withContext(Dispatchers.IO) {
             userDatabase.clearUser()
+        }
+    }
+
+    private suspend fun clearAllContacts() {
+        withContext(Dispatchers.IO) {
+            contactDatabase.clearContacts()
+        }
+    }
+
+    private suspend fun reconnectNetwork() {
+        withContext(Dispatchers.IO) {
+            Networking.start()
         }
     }
 
