4d09ee6924a6dffd2da260d05b8a13d19a5646c9
justus <beek@in.tum.de>
2023-04-25 15:45:21 +0900

    Starting network in main activity

--- app/src/main/java/com/example/apollonchat/MainActivity.kt
+++ app/src/main/java/com/example/apollonchat/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import com.example.apollonchat.configuration.Configuration
 import com.example.apollonchat.databinding.ActivityMainBinding
+import com.example.apollonchat.networking.Networking
 import com.google.android.material.appbar.AppBarLayout
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +28,10 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration.Builder(setOf(R.id.navigation_chat_list, R.id.navigation_user_creation)).build()
         // The back arrow in the actionBar (title bar on top)
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+
+        // Starting the networking service here since it will be used throughout the whole app
+        val debugConfiguration = Configuration()
+        Networking.initialize(debugConfiguration.RemoteAddress, debugConfiguration.SecureConnection)
     }
 
     override fun onSupportNavigateUp(): Boolean {
