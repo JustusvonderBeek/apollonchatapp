59eb16a9b9f72e830a70f4abcc1f3ef8101afb48
justus <beek@in.tum.de>
2023-05-09 18:43:52 +0900

    Added new raw header

--- app/src/main/java/com/example/apollonchat/networking/packets/Header.kt
+++ app/src/main/java/com/example/apollonchat/networking/packets/Header.kt
@@ -6,4 +6,20 @@ data class Header(
     var Type : Byte,
     var UserId : UInt,
     var MessageId : UInt
-)
+) {
+    fun toByteArray() : ByteArray {
+        val buffer = ByteArray(10)
+        buffer[0] = this.Category
+        buffer[1] = this.Type
+        // Big Endian
+        buffer[2] = (this.UserId shr 24).toByte()
+        buffer[3] = (this.UserId shr 16).toByte()
+        buffer[4] = (this.UserId shr 8).toByte()
+        buffer[5] = (this.UserId shr 0).toByte()
+        buffer[6] = (this.MessageId shr 24).toByte()
+        buffer[7] = (this.MessageId shr 16).toByte()
+        buffer[8] = (this.MessageId shr 8).toByte()
+        buffer[9] = (this.MessageId shr 0).toByte()
+        return buffer
+    }
+}
