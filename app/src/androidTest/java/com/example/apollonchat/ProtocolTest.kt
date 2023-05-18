f5dcd878b3110bd0faca4e592e40f4de5f5a586f
justus <beek@in.tum.de>
2023-05-19 00:14:30 +0900

    Fixed first text testing

--- app/src/androidTest/java/com/example/apollonchat/ProtocolTest.kt
+++ app/src/androidTest/java/com/example/apollonchat/ProtocolTest.kt
@@ -20,11 +20,17 @@ import java.io.File
 import java.io.IOException
 import java.nio.ByteBuffer
 import java.util.stream.Stream
+import kotlin.random.Random
+import kotlin.random.nextUInt
 
 @RunWith(AndroidJUnit4::class)
 class ProtocolTest {
 
+    private val userId = 12345u
+    private var messageId = 23768943u
+    private var contactId = 38585298u
     private lateinit var db : ApollonDatabase
+    private lateinit var random : Random
 
     @Before
     fun initDatabase() {
@@ -32,6 +38,7 @@ class ProtocolTest {
 
         db = Room.inMemoryDatabaseBuilder(context, ApollonDatabase::class.java).allowMainThreadQueries().build()
 
+        random = Random.Default
     }
 
     @After
@@ -40,42 +47,99 @@ class ProtocolTest {
         db.close()
     }
 
+    private fun createTextMessage(randomContact : Boolean = false, randomMessageId : Boolean = false, timestamp : String = "12:44:00+09:00", text : String = "Testing the protocol handler for correctness") : Pair<Header, Message> {
+        var localContactId = contactId
+        var localMessageId = messageId++
+        if (randomContact) {
+            localContactId = random.nextUInt()
+        }
+        if (randomMessageId) {
+            localMessageId = random.nextUInt()
+        }
+        val header = Header(PacketCategories.DATA.cat.toByte(), DataType.TEXT.type.toByte(), localContactId, localMessageId)
+        val message = Message(userId, timestamp, text)
+        return Pair(header, message)
+    }
+
+    private fun convertTextToRaw(text : Pair<Header, Message>) : Pair<ByteArray, ByteArray> {
+        val header = text.first
+        val message = text.second
+        val rawHeader = header.toByteArray()
+        val stringMessage = Json.encodeToString(message) + "\n"
+        val rawMessage = stringMessage.toByteArray(Charsets.UTF_8)
+        return Pair(rawHeader, rawMessage)
+    }
+
+    private fun increaseMessageId(header : ByteArray, increase : Int) : ByteArray {
+        val infoHeader = Header.convertRawToHeader(header)
+        infoHeader!!.MessageId += increase.toUInt()
+        return infoHeader!!.toByteArray()
+    }
+
     @Test
     fun testReceiveMessage() {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
         val messageDao = ApollonDatabase.getInstance(context).messageDao()
         messageDao.clearMessages()
 
-        val userId = 1234u
         ApollonProtocolHandler.initialize(userId, context.applicationContext)
 
-        val contactId = 38585298u
-        val messageId = 23768943u
-        val timestamp = "12:44:00+09:00"
-        val text = "Testing the protocol handler for correctness"
-        val header = Header(PacketCategories.DATA.cat.toByte(), DataType.TEXT.type.toByte(), contactId, messageId)
-        val message = Message(userId, timestamp, text)
+        var text = "Testing the procotol handling of chat protocol"
+        var pair = createTextMessage(text = text)
+        var raw = convertTextToRaw(pair)
+        var rawHeader = raw.first
+        var stringMessage = raw.second
 
-        val rawHeader = header.toByteArray()
-        val stringMessage = Json.encodeToString(message) + "\n"
-
-        val tmpFile = File(context.filesDir, "protocolTest.txt")
+        var tmpFile = File(context.filesDir, "protocolTest.txt")
+        tmpFile.deleteOnExit()
         // Writing the data into the file so that the method can work with it
-        val outStream = tmpFile.outputStream()
-        outStream.write(stringMessage.toByteArray(Charsets.UTF_8))
+        var outStream = tmpFile.outputStream()
+        outStream.write(stringMessage)
         outStream.flush()
 
         ApollonProtocolHandler.ReceiveAny(rawHeader, tmpFile.inputStream())
 
-        Thread.sleep(1000)
-        val messages = messageDao.getMessages(contactId.toLong())
+        // Wait for database to finish writing data into it (100ms should be enough for most)
+        Thread.sleep(100)
+        var messages = messageDao.getMessages(contactId.toLong())
 
         Assert.assertNotNull(messages)
         Assert.assertEquals(1, messages!!.size)
         Assert.assertEquals(text, messages[0].content)
+        Assert.assertEquals(1, messages[0].messageId)
+
+        // Prepare for next run
+        outStream.close()
+        tmpFile = File(context.filesDir, "protocolTest.txt")
+        tmpFile.deleteOnExit()
+        outStream = tmpFile.outputStream()
+
+        text = "Testing the protocol a second time"
+        pair = createTextMessage(text = text)
+        raw = convertTextToRaw(pair)
+        stringMessage = raw.second
+        outStream.write(stringMessage)
+        outStream.flush()
+        rawHeader = increaseMessageId(rawHeader, 1)
+
+        ApollonProtocolHandler.ReceiveAny(rawHeader, tmpFile.inputStream())
+
+        Thread.sleep(100)
+
+        messages = messageDao.getMessages(contactId.toLong())
+
+        Assert.assertNotNull(messages)
+        Assert.assertEquals(2, messages!!.size)
+//        println("Messages: ${messages[0]} & ${messages[1]}")
+        Assert.assertEquals(text, messages[1].content)
+        Assert.assertEquals(2, messages[1].messageId)
 
         outStream.close()
-        tmpFile.delete()
+    }
+
+    @Test
+    fun testWrongTextInput() {
+
     }
 
 }
