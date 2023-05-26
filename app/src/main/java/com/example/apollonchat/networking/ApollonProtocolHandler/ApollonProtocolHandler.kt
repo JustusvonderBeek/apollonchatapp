5bbf4469ab7ac1c6ad921df0dfb22b66ad7234e5
justus <beek@in.tum.de>
2023-05-26 22:39:47 +0900

    Added sending and receiving of more packets

--- app/src/main/java/com/example/apollonchat/networking/ApollonProtocolHandler/ApollonProtocolHandler.kt
+++ app/src/main/java/com/example/apollonchat/networking/ApollonProtocolHandler/ApollonProtocolHandler.kt
@@ -1,6 +1,5 @@
 package com.example.apollonchat.networking.ApollonProtocolHandler
 
-import android.app.Application
 import android.content.Context
 import android.icu.lang.UCharacter
 import android.util.Log
@@ -16,30 +15,33 @@ import com.example.apollonchat.networking.constants.ContactType
 import com.example.apollonchat.networking.constants.DataType
 import com.example.apollonchat.networking.constants.PacketCategories
 import com.example.apollonchat.networking.packets.ContactInfo
-import com.example.apollonchat.networking.packets.ContactList
 import com.example.apollonchat.networking.packets.ContactOption
+import com.example.apollonchat.networking.packets.Create
 import com.example.apollonchat.networking.packets.Header
 import com.example.apollonchat.networking.packets.Message
-import com.example.apollonchat.networking.packets.NetworkContact
 import com.example.apollonchat.networking.packets.NetworkOption
+import com.example.apollonchat.networking.packets.Search
 import com.github.luben.zstd.Zstd
 import io.ktor.util.date.getTimeMillis
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonDecoder
 import java.io.File
 import java.io.InputStream
-import java.lang.Exception
 import java.time.LocalDateTime
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.random.Random
 import kotlin.random.nextUInt
+import kotlin.time.Duration
 
 /*
 * Instead of using a class we use an object.
@@ -52,6 +54,7 @@ object ApollonProtocolHandler {
 
     private var userId : UInt = 0U
     private var randomness : Random = Random(LocalDateTime.now().nano)
+    private var timeout = 2000L
     private var messageID : AtomicInteger = AtomicInteger(randomness.nextInt())
     private var unackedPackets : MutableList<Pair<UInt, ByteArray>> = ArrayList()
     // TODO: What is this for?
@@ -59,6 +62,7 @@ object ApollonProtocolHandler {
     private var ignoreUnknownJson = Json { ignoreUnknownKeys = true }
     private var imageFileDir : String? = null
     private var user : User? = null
+    private var userDatabase : UserDatabaseDao? = null
     private var messageDatabase : MessageDao? = null
     private var contactDatabase : ContactDatabaseDao? = null
 
@@ -75,9 +79,9 @@ object ApollonProtocolHandler {
     fun initialize(userId : UInt, context: Context) {
         this.imageFileDir = context.filesDir.absolutePath
         val database = ApollonDatabase.getInstance(context)
-        val userDatabase = database.userDao()
+        userDatabase = database.userDao()
         protocolScope.launch {
-            LoadUser(userDatabase)
+            LoadUser(userDatabase!!)
         }
         contactDatabase = database.contactDao()
         messageDatabase = database.messageDao()
@@ -90,32 +94,73 @@ object ApollonProtocolHandler {
         }
     }
 
+    private fun readFullLine(incoming : InputStream, timeout : Long) : String? {
+        try {
+            val payload = incoming.bufferedReader().readLine()
+//            val payload = runBlocking {
+//                withTimeoutOrNull(Duration.parse("1s")) {
+//                    val finalString = StringBuilder()
+//                    val bufRead = incoming.bufferedReader()
+//                    // TODO: Read all until delimiter and timeout
+////                    do {
+////                        val json = bufRead.readText()
+////                        finalString.append(json)
+////                    } while (!json.endsWith("\n") && !json.endsWith("\r\n"))
+//                    Log.i("ApollonProtocolHandler", "Starting to read string data")
+//                    val json = bufRead.readText()
+//                    finalString.append(json)
+//                    Log.i("ApollonProtocolHandler", "Finished reading protocol data")
+//                    bufRead.close()
+//                    finalString.toString()
+//                }
+//            }
+            return payload
+        } catch (ex : Exception) {
+            Log.i("ApollonProtocolHandler", "Failed to receive! $ex")
+            return null
+        }
+    }
 
     // Main method for initial packet handling
-    fun ReceiveAny(headerBuffer : ByteArray, incomingStream: InputStream) {
+    fun receiveAny(headerBuffer : ByteArray, incomingStream: InputStream) {
         val header = Header.convertRawToHeader(headerBuffer) ?: return
+        Log.i("ApollonProtocolHandler", "Header: $header")
         // We cannot always read payload, because of packets that don't have a payload
         when(header.Category.toLong()) {
             PacketCategories.CONTACT.cat.toLong() -> {
                 when(header.Type.toLong()) {
+                    ContactType.CREATE.type.toLong() -> {
+                        // Not expecting any payload but need to create a new user in the database
+                        if (userDatabase == null) {
+                            Log.i("ApollonProtocolHandler", "Database is null. Cannot insert new user!")
+                            return
+                        }
+                        val newUser = User(header.UserId.toLong(), "TODO", userImage = "drawable/usericon.png")
+                        // TODO: BUGFIX might running on wrong thread and leading to starvation
+                        userDatabase!!.insertUser(newUser)
+                        Log.i("ApollonProtocolHandler", "Wrote user into database")
+                    }
                     ContactType.CONTACTS.type.toLong() -> {
                         // Send this further to the contact view model?
                         // Or maybe let this one handle this alone?
                         // We need to "clean" the pipe, so read anyways
-                        val payload = incomingStream.bufferedReader(Charsets.UTF_8).readLine()
+                        Log.i("ApollonProtocolHandler", "Received Contacts...")
+//                        val payload = readFullLine(incomingStream, timeout) ?: return
+                        val payload = incomingStream.bufferedReader().readLine()
+                        Log.i("ApollonProtocolHandler", "Showing the contacts via the callback")
                         if (contactsCallback != null) {
                             contactsCallback!!.invoke(payload)
                         }
                     }
                     ContactType.OPTION.type.toLong() -> {
                         // Differentiate further
-                        val payload = incomingStream.bufferedReader(Charsets.UTF_8).readLine()
+                        val payload = readFullLine(incomingStream, timeout) ?: return
                         protocolScope.launch {
                             receiveContactOption(header, payload)
                         }
                     }
                     ContactType.CONTACT_INFO.type.toLong() -> {
-                        val payload = incomingStream.bufferedReader(Charsets.UTF_8).readLine()
+                        val payload = readFullLine(incomingStream, timeout) ?: return
                         protocolScope.launch {
                             receiveContactInformation(header, payload)
                         }
@@ -136,13 +181,17 @@ object ApollonProtocolHandler {
                 when(header.Type.toLong()) {
                     DataType.TEXT.type.toLong() ->  {
                         // Receive only payload and then free the stream to continue receiving
-                        val payload = incomingStream.bufferedReader(Charsets.UTF_8).readLine()
+//                        val payload = readFullLine(incomingStream, timeout) ?: return
+                        val payload = incomingStream.bufferedReader().readLine()
+                        Log.i("ApollonProtocolHandler", "Received text")
                         protocolScope.launch {
                             receiveTextMessage(header, payload)
                         }
                     }
                     DataType.TEXT_ACK.type.toLong() -> {
                         // Extract the messageID that was acked
+                        // empty the stream for now
+                        incomingStream.bufferedReader().readLine()
                         val ackedID = header.MessageId
                         unackedPackets.removeIf {
                                 pair -> pair.first == ackedID
@@ -161,6 +210,27 @@ object ApollonProtocolHandler {
         }
     }
 
+    private suspend fun insertMessageIntoDatabase(header: Header, message: Message) {
+        withContext(Dispatchers.IO) {
+            var prevId = 0L
+            val messages = messageDatabase!!.getMessages(message.ContactUserId.toLong())
+            if (messages != null) {
+                prevId = messages.size + 1L
+            }
+            val localMessage = DisplayMessage(prevId, message.ContactUserId.toLong(), true, message.Message, message.Timestamp)
+            messageDatabase!!.insertMessage(localMessage)
+
+            val contact = contactDatabase!!.getContact(header.UserId.toLong())
+            if (contact == null) {
+                Log.i("ApollonProtocolHandler", "Failed to find user for ID ${header.UserId}")
+                // Should kill the connection?
+                return@withContext
+            }
+            contact.lastMessage = message.Message
+            contactDatabase!!.updateContact(contact)
+        }
+    }
+
     // TODO: Implement missing parts of the protocol
     fun SendText(text : String, to : UInt) {
         try {
@@ -173,7 +243,12 @@ object ApollonProtocolHandler {
             val mID = messageID.getAndAdd(1).toUInt()
             val header = Header(PacketCategories.DATA.cat.toByte(), DataType.TEXT.type.toByte(), userId, mID)
             val message = Message(to, getTimeMillis().toString(), text)
-            SendAny(mID, header, message)
+            SendAny(header, message)
+
+            // Adding the text into the message database
+            protocolScope.launch {
+                insertMessageIntoDatabase(header, message)
+            }
         } catch (ex : NullPointerException) {
             Log.i("ApollonProtocolHandler", "Internal programming error!")
             ex.printStackTrace()
@@ -186,8 +261,42 @@ object ApollonProtocolHandler {
 
     }
 
-    fun SendCreateAccount() {
+    fun SendCreateAccount(newUser : User) {
+        try {
+            if (newUser.username.isEmpty()) {
+                Log.i("ApollonProtocolHandler", "Cannot create user with empty name")
+                return
+            }
+            val create = Create(newUser.username)
+            val mID = messageID.getAndAdd(1).toUInt()
+            val header = Header(PacketCategories.CONTACT.cat.toByte(), ContactType.CREATE.type.toByte(), 0u, mID)
+            SendAny(header, create)
+        } catch (ex : Exception) {
+            Log.i("ApollonProtocolHandler", "Failed to send create account to server!\n$ex")
+        }
+    }
 
+    fun SendSearch(searchString: String) {
+        try {
+            val search = Search(searchString)
+            val mID = messageID.getAndAdd(1).toUInt()
+            val header = Header(PacketCategories.CONTACT.cat.toByte(), ContactType.SEARCH.type.toByte(), userId, mID)
+            SendAny(header, search)
+        } catch (ex : Exception) {
+            Log.i("ApollonProtocolHandler", "Failed to send search to server!\n$ex")
+        }
+    }
+
+    fun SendContactRequest(contactId : UInt) {
+        try {
+            val addOption = listOf(NetworkOption("Question", "Add"))
+            val option = ContactOption(contactId, addOption)
+            val mID = messageID.getAndAdd(1).toUInt()
+            val header = Header(PacketCategories.CONTACT.cat.toByte(), ContactType.OPTION.type.toByte(), userId, mID)
+            SendAny(header, option)
+        } catch (ex : Exception) {
+            Log.i("ApollonProtocolHandler", "Failed to send search to server!\n$ex")
+        }
     }
 
     fun registerContactsCallback(callback : (String) -> Unit) {
@@ -199,13 +308,33 @@ object ApollonProtocolHandler {
     // Private API
     // ---------------------------------------------------
 
+    private fun SendAny(header : Header, payload : Search) {
+        val rawPayload = ignoreUnknownJson.encodeToString(payload)
+        SendAny(header, rawPayload)
+    }
+
+    private fun SendAny(header : Header, payload : Create) {
+        val rawPayload = ignoreUnknownJson.encodeToString(payload)
+        SendAny(header, rawPayload)
+    }
+
+    private fun SendAny(header: Header, payload: Message) {
+        val rawPayload = ignoreUnknownJson.encodeToString(payload)
+        SendAny(header, rawPayload)
+    }
+
+    private fun SendAny(header: Header, payload: ContactOption) {
+        val rawPayload = ignoreUnknownJson.encodeToString(payload)
+        SendAny(header, rawPayload)
+    }
+
     // TODO: Divide visually from receive methods
-    private fun SendAny(messageId : UInt, header : Header, payload : Any) {
+    private fun SendAny(header : Header, payload : String) {
         // Get the message ID of the packet to ack later
-        val rawPayload = ignoreUnknownJson.encodeToString(payload) + "\n"
+        val rawPayload = payload + "\n"
         val rawHeader = header.toByteArray()
         val packet = rawHeader + rawPayload.toByteArray(Charsets.UTF_8)
-        val pair = Pair(messageId, packet)
+        val pair = Pair(header.MessageId, packet)
         unackedPackets.add(pair)
         Networking.write(packet)
     }
@@ -236,8 +365,17 @@ object ApollonProtocolHandler {
         if (contactDatabase == null || messageDatabase == null) {
             throw IllegalStateException("Protocol Handler not initialized! Wrong usage!")
         }
+        Log.i("ApollonProtocolHandler", "Payload: $payload")
         withContext(Dispatchers.IO) {
-            val message = ignoreUnknownJson.decodeFromString<Message>(payload)
+            // Use the default JSON here because we only want to accept the correct Message here!
+            val res = kotlin.runCatching {
+                Json.decodeFromString<Message>(payload)
+            }
+            if (res.isFailure) {
+                Log.i("ApollonProtocolHandler", "Failed to decode text payload")
+                return@withContext
+            }
+            val message = res.getOrNull()!!
             // Inserting the new message into the database
             var prevId = 0L
             val messages = messageDatabase!!.getMessages(header.UserId.toLong())
@@ -307,7 +445,7 @@ object ApollonProtocolHandler {
             val contactId = header.UserId
             // Flip the IDs and send back
             header.UserId = userId
-            SendAny(header.MessageId, header, answer)
+            SendAny(header, answer)
 
             // Saving the new contact in the list of contacts
             val imageFile = File(imageFileDir, "${contactId}.png")
