0dd9f72dc5fe3ea4017e3ba904abb12e7b1cda24
justus <beek@in.tum.de>
2023-06-06 20:39:29 +0900

    Restructured some parts

--- app/src/main/java/com/example/apollonchat/networking/Networking.kt
+++ app/src/main/java/com/example/apollonchat/networking/Networking.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.sync.Semaphore
 import java.io.IOException
 import java.io.InputStream
+import java.net.Inet4Address
 import java.net.InetAddress
 import java.util.Hashtable
 import kotlin.concurrent.thread
@@ -22,12 +23,12 @@ import kotlin.reflect.KSuspendFunction2
 object Networking {
 
     class Configuration (
-        var remote : InetAddress,
+        var remote : String,
         var headerSize : Int,
         var secure : Boolean,
     ) {
-//        constructor() : this(InetAddress.getByName("178.1.205.20"), 10, true)
-        constructor() : this(InetAddress.getByName("10.0.2.2"), 10, false)
+//        constructor() : this("homecloud.homeplex.org", 10, true)
+        constructor() : this("10.0.2.2", 10, false)
     }
 
     /*
@@ -60,11 +61,9 @@ object Networking {
     private var networkingJob = Job()
     private val netScope = CoroutineScope(Dispatchers.Main + networkingJob)
 
-    private var registeredCallbacks = Hashtable<Pair<Long, Long>, MutableList<(String, InputStream) -> Unit>>()
-    private var defaultCallback = mutableListOf<KSuspendFunction2<ByteArray, InputStream, Unit>>()
-
     // Configuration
-    private var remoteAddress : InetAddress? = null
+    private var remoteAddress : String? = null
+    private var remotePort : Int = 0
     private var connectSecure : Boolean = false
     private var headerSize : Int = 10
 
@@ -78,6 +77,7 @@ object Networking {
         if (!init) {
             remoteAddress = configuration.remote
             connectSecure = configuration.secure
+            remotePort = if (connectSecure) 50001 else 50000
             headerSize = configuration.headerSize
             init = true
         }
@@ -92,14 +92,18 @@ object Networking {
         connect(context)
 
         // Then starting to send and receiver
-        if (sending == null || !sending!!.isAlive) {
+        if (sending == null) {
             sending = thread {
-                netScope.launch { startSending() }
+                runBlocking {
+                    startSending()
+                }
             }
         }
-        if (receiving == null || !receiving!!.isAlive) {
+        if (receiving == null) {
             receiving = thread {
-                netScope.launch { startListening() }
+                runBlocking {
+                    startListening()
+                }
             }
         }
     }
@@ -109,20 +113,6 @@ object Networking {
         netScope.launch {
             outputChannel.send(data)
         }
-    }
-
-    // TODO: Restructure to allow for injection of packet handling routine
-    fun registerCallback(category : Long, type : Long, callback : (String, InputStream) -> Unit) {
-        if (registeredCallbacks[Pair(category, type)] == null) {
-            registeredCallbacks[Pair(category, type)] = mutableListOf(callback)
-        } else {
-            registeredCallbacks[Pair(category, type)]!!.add(callback)
-        }
-    }
-
-    // TODO: Remove and combine in above function
-    fun registerCallback(callback: KSuspendFunction2<ByteArray, InputStream, Unit>) {
-        defaultCallback.add(callback)
     }
 
     /*
@@ -145,19 +135,17 @@ object Networking {
 //        Log.i("Networking", "After connection launch")
     }
 
-    // TODO: Cleanup
+    // TODO: Fix waiting, signalling
     private suspend fun connectDefault() {
-        if (connected) {
-            Log.i("Networking", "Already connected")
+        if (connected && socket != null && !socket!!.isClosed) {
+            Log.i("Networking", "Already TCP connected")
             return
         }
-         val con = withContext(Dispatchers.IO) {
+         connected = withContext(Dispatchers.IO) {
             val selManager = SelectorManager(Dispatchers.IO)
             try {
-//                socket = aSocket(selManager).tcp().connect("homecloud.homeplex.org", port = 50000)
-                // This address should emulate the localhost address
-                socket = aSocket(selManager).tcp().connect(remoteAddress!!.hostAddress!!, port = 50000)
-//                        socket = aSocket(selManager).tcp().connect("192.168.178.53", port = 50000)
+                socket = aSocket(selManager).tcp().connect(InetAddress.getByName(remoteAddress).hostAddress!!, port = remotePort)
+
                 Log.i("Networking", "Connected to remote per TCP")
                 // TODO: Implement proper barrier instead of this Voudu shit
                 initBarrier.release()
@@ -168,19 +156,21 @@ object Networking {
                 return@withContext false
             }
         }
-        connected = con
     }
 
     // TODO: Cleanup
     private suspend fun connectSecure(context: Context) {
-        val con = withContext(Dispatchers.IO) {
-            // The TLS variant
+        if (connected && socket != null && !socket!!.isClosed) {
+            Log.i("Networking", "Already TLS connected")
+            return
+        }
+        connected = withContext(Dispatchers.IO) {
             try {
                 val selManager = SelectorManager(Dispatchers.IO)
+                // The TLS variant
                 val tlsConfig = ApollonNetworkConfigCreator.createTlsConfig(context.resources.openRawResource(R.raw.apollon))
                 // If the context is set as 'coroutineContext' then the method DOES NOT return back to the caller side!
-                socket = aSocket(selManager).tcp().connect(remoteAddress!!.hostAddress!!, port = 50001).tls(Dispatchers.IO, tlsConfig)
-//                socket = aSocket(selManager).tcp().connect("10.0.2.2", port = 50001).tls(Dispatchers.IO, tlsConfig)
+                socket = aSocket(selManager).tcp().connect(InetAddress.getByName(remoteAddress).hostAddress!!, port = remotePort).tls(Dispatchers.IO, tlsConfig)
 
                 Log.i("Networking", "Connected to remote per TLS")
                 initBarrier.release()
@@ -191,19 +181,10 @@ object Networking {
                 return@withContext false
             }
         }
-        connected = con
     }
 
     // Move to own section
     private fun ByteArray.toHexString() = joinToString("") { "%02x".format(it) }
-    private fun Int.to2ByteArray() : ByteArray = byteArrayOf(shr(8).toByte(), toByte())
-
-    // Expecting Big Endian
-    private fun ByteArray.toUInt16(): UInt {
-        val upper = this[0].toUByte()
-        val lower = this[1].toUByte()
-        return (upper.toUInt() shl 8) + lower.toUInt()
-    }
 
     /*
     ----------------------------------------------------------------
@@ -214,12 +195,14 @@ object Networking {
     // TODO: Test, Error handling
     private suspend fun startSending() {
         if (sending != null && sending!!.isAlive) {
-            Log.i("Networking", "Already sending")
-            return
+            if (sending != Thread.currentThread()) {
+                Log.i("Networking", "Already sending")
+                return
+            }
         }
         initBarrier.acquire()
         Log.i("Networking", "Starting to send...")
-        val con = withContext(Dispatchers.IO) {
+        withContext(Dispatchers.IO) {
             try {
                 val sendChannel = socket!!.openWriteChannel(autoFlush = true)
                 while (true) {
@@ -243,8 +226,10 @@ object Networking {
 //     including size of header field to be expected
      private suspend fun startListening() {
         if (receiving != null && receiving!!.isAlive) {
-            Log.i("Networking", "Already receiving")
-            return
+            if (receiving != Thread.currentThread()) {
+                Log.i("Networking", "Already receiving")
+                return
+            }
         }
         initBarrier.acquire()
         Log.i("Networking", "Starting to receive...")
@@ -263,11 +248,6 @@ object Networking {
                     ApollonProtocolHandler.receiveAny(headerBuffer, recChannel)
 //                    defaultCallback[0].invoke(headerBuffer, recChannel)
 //                    defaultCallback[0].invoke(packetBuffer, recChannel)
-//                    registeredCallbacks[Pair(header.Category.toLong(), header.Type.toLong())]?.let {
-//                        for(cal in it) {
-//                            cal.invoke(sPacket, recChannel)
-//                        }
-//                    }
                 }
             } catch (ex : NullPointerException) {
                 Log.i("Networking", "Failed to get read channel for remote: ${ex.printStackTrace()}")
