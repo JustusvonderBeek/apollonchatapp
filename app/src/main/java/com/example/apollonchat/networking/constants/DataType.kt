64202f4a80de91bccf170e892a9074f064670ad3
justus <beek@in.tum.de>
2023-06-08 00:03:28 +0900

    Include new file transfer types

--- app/src/main/java/com/example/apollonchat/networking/constants/DataType.kt
+++ app/src/main/java/com/example/apollonchat/networking/constants/DataType.kt
@@ -3,4 +3,8 @@ package com.example.apollonchat.networking.constants
 enum class DataType(val type : Int) {
     TEXT(1),
     TEXT_ACK(2),
+    FILE_INFO(3),
+    FILE_HAVE(4),
+    FILE(5),
+    FILE_ACK(6),
 }
