7b59ff39cf8b3d4bfbc0f930862389fa1d6cba90
justus <beek@in.tum.de>
2023-06-20 21:37:43 +0900

    Cleanup of factory

--- app/src/main/java/com/example/apollonchat/usercreate/UserCreationViewModelFactory.kt
+++ app/src/main/java/com/example/apollonchat/usercreate/UserCreationViewModelFactory.kt
@@ -3,14 +3,16 @@ package com.example.apollonchat.usercreate
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.apollonchat.database.ApollonDatabase
 import com.example.apollonchat.database.contact.ContactDatabaseDao
 import com.example.apollonchat.database.user.UserDatabaseDao
 
-class UserCreationViewModelFactory(val database : UserDatabaseDao, val application: Application) : ViewModelProvider.Factory {
+class UserCreationViewModelFactory(val application: Application) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(UserCreationViewModel::class.java)) {
-            return UserCreationViewModel(database, application) as T
+            val userDb = ApollonDatabase.getInstance(application).userDao()
+            return UserCreationViewModel(userDb, application) as T
         }
         throw java.lang.IllegalArgumentException("Unknown init class")
     }
