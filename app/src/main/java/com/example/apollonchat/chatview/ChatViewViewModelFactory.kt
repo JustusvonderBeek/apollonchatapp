20989d5beeb00db07d0f8226b308e8c5105ba44a
justus <beek@in.tum.de>
2023-06-20 19:59:57 +0900

    Cleanup

--- app/src/main/java/com/example/apollonchat/chatview/ChatViewViewModelFactory.kt
+++ app/src/main/java/com/example/apollonchat/chatview/ChatViewViewModelFactory.kt
@@ -3,15 +3,19 @@ package com.example.apollonchat.chatview
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.apollonchat.database.ApollonDatabase
 import com.example.apollonchat.database.contact.ContactDatabaseDao
 import com.example.apollonchat.database.message.MessageDao
 import com.example.apollonchat.database.user.UserDatabaseDao
 
-class ChatViewViewModelFactory(val contactID : Long, val database : ContactDatabaseDao, val uDatabase : UserDatabaseDao, val mDatabase : MessageDao, val application: Application) : ViewModelProvider.Factory {
+class ChatViewViewModelFactory(val contactID : Long, val application: Application) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ChatViewViewModel::class.java)) {
-            return ChatViewViewModel(contactID, database, uDatabase, mDatabase, application) as T
+            val contactDb = ApollonDatabase.getInstance(application).contactDao()
+            val userDb = ApollonDatabase.getInstance(application).userDao()
+            val messageDb = ApollonDatabase.getInstance(application).messageDao()
+            return ChatViewViewModel(contactID, contactDb, userDb, messageDb, application) as T
         }
         throw java.lang.IllegalArgumentException("Unknown init class")
     }
